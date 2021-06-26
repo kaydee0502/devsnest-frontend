e0e8305ff46d891b45700e4d3198c08464d48956
kaydee0502 <kshitijdhama513@gmail.com>
2021-06-26 16:14:30 +0530

    tha 11 done

--- THA11/script.js
+++ THA11/script.js
@@ -1,6 +1,6 @@
 const ques = 10;
 //const fetch = require('node-fetch');
-
+let score = 0
 let url = "https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple"
 
 let mainData = null
@@ -27,10 +27,13 @@ mainData.then((response) => {
     console.log(a)
     let j = 0
     for (let k = 1; k<=4; k++){
+        let e = document.getElementsByClassName(`option${k}`)[0]
+        e.addEventListener("click", (elm) => opclick(e));
+        
         if (k === rndInd){
             continue
         }
-        document.getElementsByClassName(`option${k}`)[0].innerHTML = response.results[i].incorrect_answers[j]
+        e.innerHTML = response.results[i].incorrect_answers[j]
         j+=1
     }
     i++;
@@ -40,7 +43,21 @@ mainData.then((response) => {
 )
 
 function next(){
+    if (i >= ques){
+        alert(`go home bruh! your score was ${score}`)
+    }
     document.getElementsByClassName("pre_correct")[0].classList.remove("pre_correct")
+    let cs = document.getElementsByClassName("correct")[0] 
+    let ws = document.getElementsByClassName("wrong")[0]
+    if (cs !== undefined){
+        cs.classList.remove("correct")
+        console.log(cs)
+
+    }
+    if (ws !== undefined){
+        ws.classList.remove("wrong")
+        console.log(ws)
+    }
 
     document.getElementById('question').innerHTML = mainData[i].question
     const rndInd = Math.floor(Math.random() * 4) + 1
@@ -50,16 +67,35 @@ function next(){
     console.log(a)
     let j = 0
     for (let k = 1; k<=4; k++){
+        let e = document.getElementsByClassName(`option${k}`)[0]
+      
         if (k === rndInd){
             continue
         }
-        document.getElementsByClassName(`option${k}`)[0].innerHTML = mainData[i].incorrect_answers[j]
+        e.innerHTML = mainData[i].incorrect_answers[j]
         j+=1
     }
     i++;
 }
 
-function opclick(){}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+async function opclick(option){
+    console.log(option)
+    if (option.classList.contains("pre_correct")){
+        option.classList.add("correct")
+        score++
+    }
+    else{
+        option.classList.add("wrong")
+        document.getElementsByClassName("pre_correct")[0].classList.add("correct")
+    }
+
+    await sleep(2000)
+    next()
+}
 
 
 
