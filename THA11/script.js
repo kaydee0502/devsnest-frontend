2cd0f01786b77656d96c233a148a41eb539e124b
kaydee0502 <kshitijdhama513@gmail.com>
2021-06-26 15:30:40 +0530

    pre script

--- THA11/script.js
+++ THA11/script.js
@@ -3,32 +3,63 @@ const ques = 10;
 
 let url = "https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple"
 
-let dat = null
-fetch(url)
-  .then( res => res.json() )
-  .then( data => {
+let mainData = null
 
-    
+async function getData(){
+   const response = await fetch(url);
+   const body = await response.json();
+   return body
 
-    console.log(typeof data.results)
-    let i = 0
+}
 
 
-    
-    for (const i in data.results) {
-        console.log(data.results[i].question)
-        document.getElementById("question").innerHTML = data.results[i].question
+mainData = getData()
+//setTimeout(() => {},2000)
+let i = 0
+mainData.then((response) => {
+
+    console.log(response)
+    document.getElementById('question').innerHTML = response.results[i].question
+    const rndInd = Math.floor(Math.random() * 4) + 1
+    let a = document.getElementsByClassName(`option${rndInd}`)[0]
+    a.innerHTML = response.results[i].correct_answer
+    a.classList.add("pre_correct")
+    console.log(a)
+    let j = 0
+    for (let k = 1; k<=4; k++){
+        if (k === rndInd){
+            continue
+        }
+        document.getElementsByClassName(`option${k}`)[0].innerHTML = response.results[i].incorrect_answers[j]
+        j+=1
     }
+    i++;
+    mainData = response.results
+}
 
+)
 
-  } );
+function next(){
+    document.getElementsByClassName("pre_correct")[0].classList.remove("pre_correct")
 
+    document.getElementById('question').innerHTML = mainData[i].question
+    const rndInd = Math.floor(Math.random() * 4) + 1
+    let a = document.getElementsByClassName(`option${rndInd}`)[0]
+    a.innerHTML = mainData[i].correct_answer
+    a.classList.add("pre_correct")
+    console.log(a)
+    let j = 0
+    for (let k = 1; k<=4; k++){
+        if (k === rndInd){
+            continue
+        }
+        document.getElementsByClassName(`option${k}`)[0].innerHTML = mainData[i].incorrect_answers[j]
+        j+=1
+    }
+    i++;
+}
 
-
-
-
-
-
+function opclick(){}
 
 
 
