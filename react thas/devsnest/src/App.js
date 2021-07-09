8124f6da0134a15ba66f61bbc6b1dd43200202b4
kaydee0502 <kshitijdhama513@gmail.com>
2021-07-09 11:13:17 +0530

    added routing

--- react thas/devsnest/src/App.js
+++ react thas/devsnest/src/App.js
@@ -1,18 +1,19 @@
 import {Component} from 'react'
 // import ChessBoard from "./chessBoard/ChessBoard";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 import MemeCard from "./memeCard/memeCard";
-
-class App extends Component{
-  render(){
-    return (
-        <>
-            <MemeCard/>
-            {/* <ChessBoard /> */}
-        </>
-    )
-  }
+// import Day17 from "./components/day17";
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/day16">
+          <MemeCard />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
-
-
